cb67d17368ef77195733c260378dea0640b390ec
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-11-07 15:35:24 +0000

    Using js create trail when mouse hovers over grid that fades to white after period of time

--- EaS.js
+++ EaS.js
@@ -1,10 +1,26 @@
 function createGrid(squares) {
     for(let i = squares; i > 0; i--){
-        const square = document.createElement('div')
-        square.style.cssText = 'border:1px solid black; height:25px; width:25px;'
-        square.classList.add('square')
+        const square = document.createElement('div');
+        square.style.cssText = 'border:1px solid black; height:25px; width:25px;';
+        square.classList.add('square');
         container.appendChild(square);
     }
 }
-
 createGrid(Math.pow(16,2));
+
+const gridSquare = document.querySelectorAll('.square');
+gridSquare.forEach(square => square.addEventListener('mouseenter',hoverEnter));
+gridSquare.forEach(square => square.addEventListener('mouseleave',hoverLeave));
+
+function hoverEnter(e){
+    e.target.setAttribute('class','hover');
+}
+
+function hoverLeave(e){
+    e.target.setAttribute('class','hoverExit hover');
+    e.target.addEventListener('animationend', hoverTrail);
+}
+
+function hoverTrail(e) {
+    e.target.setAttribute('class','none');
+}
