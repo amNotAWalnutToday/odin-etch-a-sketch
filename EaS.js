2b7e8e312443d43a619dc2eec020d2764c75ccfa
unknown <117041148+amNotAWalnutToday@users.noreply.github.com>
2022-11-07 17:06:51 +0000

    Add functioning generate grid button

--- EaS.js
+++ EaS.js
@@ -1,26 +1,53 @@
+//creating the grid
+const container = document.querySelector('#container')
+
 function createGrid(squares) {
+const lines = Math.sqrt(squares);
+const squareSize = container.offsetWidth / lines;
+    console.log(squareSize);
     for(let i = squares; i > 0; i--){
         const square = document.createElement('div');
-        square.style.cssText = 'border:1px solid black; height:25px; width:25px;';
+        square.style.cssText = `border:1px solid black; height:${squareSize - 2}px; width:${squareSize - 2}px;`;
         square.classList.add('square');
         container.appendChild(square);
     }
 }
 createGrid(Math.pow(16,2));
+setTrail();
 
-const gridSquare = document.querySelectorAll('.square');
-gridSquare.forEach(square => square.addEventListener('mouseenter',hoverEnter));
-gridSquare.forEach(square => square.addEventListener('mouseleave',hoverLeave));
+
+//creating the trail
+
+function setTrail() {
+    const gridSquare = document.querySelectorAll('.square');
+    gridSquare.forEach(square => square.addEventListener('mouseenter',hoverEnter));
+    gridSquare.forEach(square => square.addEventListener('mouseleave',hoverLeave));
+}
 
 function hoverEnter(e){
-    e.target.setAttribute('class','hover');
+    this.setAttribute('class','hover');
 }
 
 function hoverLeave(e){
-    e.target.setAttribute('class','hoverExit hover');
-    e.target.addEventListener('animationend', hoverTrail);
+    this.setAttribute('class','hoverExit hover');
+    this.addEventListener('animationend', hoverTrail);
 }
 
 function hoverTrail(e) {
-    e.target.setAttribute('class','none');
+    this.setAttribute('class','none');
+}
+
+//generate new grid
+const button = document.querySelector('#generate')
+button.addEventListener('click', generateGrid);
+
+function clearGrid(){
+    const gridSquare = document.querySelectorAll('.square');
+    gridSquare.forEach(square => container.removeChild(square));
+}
+
+function generateGrid(e) {
+    clearGrid();
+    createGrid(Math.pow(prompt('Enter amount of grid lines'),2));
+    setTrail();
 }
